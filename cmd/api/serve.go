1c1efcbcf316aecaf77027bafad44cb5c41d675f
Motiur Rahman <mdmotiurrahmansany@gmail.com>
2026-03-21 22:11:30 +0600

    Implement graceful server shutdown and enhance logging in serve function

--- cmd/api/serve.go
+++ cmd/api/serve.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
+	"log/slog"
 	"net/http"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/MotiurRahmanSany/url-shrinker-api/internal/api/handlers"
@@ -17,14 +22,21 @@ import (
 	"github.com/MotiurRahmanSany/url-shrinker-api/internal/service"
 )
 
+var (
+	ServerReadHeaderTimeout = 5 * time.Second
+	ServerReadTimeout       = 5 * time.Second
+	ServerWriteTimeout      = 10 * time.Second
+	ServerIdleTimeout       = 30 * time.Second
+	ServerShutdownTimeout   = 10 * time.Second
+)
+
 func serve(config *config.Config) {
 	// connecting to database
 	pool, err := database.NewConnection(config.Db)
 	if err != nil {
-		fmt.Printf("Error connecting to database: %v\n", err.Error())
+		slog.Error("database connection failed", "err", err)
 		return
 	}
-	defer pool.Close()
 	queries := db.New(pool)
 
 	redisCache := cache.NewRedisCache(
@@ -60,11 +72,49 @@ func serve(config *config.Config) {
 
 	loggedMux := middleware.Logger(mux)
 
-	fmt.Printf("Server is running on port %d\n", config.HttpPort)
-	fmt.Printf("Base Url is: http:localhost:%d\n", config.HttpPort)
-
-	if err := http.ListenAndServe(fmt.Sprintf(":%d", config.HttpPort), loggedMux); err != nil {
-		fmt.Printf("Error starting server: %v\n", err.Error())
+	server := &http.Server{
+		Addr:              fmt.Sprintf(":%d", config.HttpPort),
+		Handler:           loggedMux,
+		ReadHeaderTimeout: ServerReadHeaderTimeout,
+		ReadTimeout:       ServerReadTimeout,
+		WriteTimeout:      ServerWriteTimeout,
+		IdleTimeout:       ServerIdleTimeout,
 	}
+
+	// Starting the server
+	go func() {
+		slog.Info("server starting", "port", config.HttpPort, "base_url", fmt.Sprintf("http://localhost:%d", config.HttpPort))
+
+		if err := server.ListenAndServe(); err != nil {
+			if errors.Is(err, http.ErrServerClosed) {
+				slog.Info("server closed")
+			} else {
+				slog.Error("server failed", "err", err)
+			}
+		}
+	}()
+
+	rootCtx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	<-rootCtx.Done()
+
+	slog.Info("shutdown signal received")
+
+	// Graceful shutdown with a timeout context
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), ServerShutdownTimeout)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		slog.Error("graceful shutdown failed", "err", err)
+	}
+
+	// Close resources
+	pool.Close()
+
+	if err := redisCache.Close(); err != nil {
+		slog.Error("redis close failed", "err", err)
+	}
+
+	slog.Info("server gracefully stopped")
 
 }
